ff68116f0af522839162a6285ab7092f3f222924
Jonas Wagner <jwagner@raptus.com>
2012-11-02 04:34:12 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.5'
 setup(name='pcommerce.email',
       version=version,
       description="",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='pcommerce.email',
       keywords='plone ecommerce pcommerce shop payment',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/pcommerce.email',
+      url='https://github.com/Raptus/pcommerce.email',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['pcommerce'],
